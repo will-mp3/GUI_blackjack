ea65c9aa16649365c79a474715f5d84501350426
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-31 17:46:10 -0500

    added instruction frame and welcome label

--- view/instructionFrame.py
+++ view/instructionFrame.py
@@ -1,1 +1,13 @@
 import tkinter as tk
+
+class InstructionFrame:
+
+    def __init__(self):
+        self.root = tk.Tk()
+
+        self.label = tk.Label(self.root, text="WELCOME TO BLACKJACK", font=("Arial", 20))
+        self.label.pack(padx=10, pady=10)
+
+        self.root.mainloop()
+
+InstructionFrame()
