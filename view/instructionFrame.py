a747a949974cf4f86f69a8455de00a5d885cded7
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-31 19:04:21 -0500

    added instruction text box

--- view/instructionFrame.py
+++ view/instructionFrame.py
@@ -8,6 +8,11 @@ class InstructionFrame:
         self.label = tk.Label(self.root, text="WELCOME TO BLACKJACK", font=("Arial", 20))
         self.label.pack(padx=10, pady=10)
 
+        self.instructions = tk.Text(self.root, height=10, width=40, wrap="word")
+        self.instructions.insert("1.0", "Fill Instructions Here")
+        self.instructions.config(state=tk.DISABLED)
+        self.instructions.pack(pady=10)
+
         self.button = tk.Button(self.root, text="Lets Go!", font=("Arial", 18), command=self.close)
         self.button.pack(padx=10, pady=10)
 
