fcebe5a58b7ecf50b9c5506080ef67b4841dabaf
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-31 19:01:03 -0500

    added close function and close button

--- view/instructionFrame.py
+++ view/instructionFrame.py
@@ -8,6 +8,12 @@ class InstructionFrame:
         self.label = tk.Label(self.root, text="WELCOME TO BLACKJACK", font=("Arial", 20))
         self.label.pack(padx=10, pady=10)
 
+        self.button = tk.Button(self.root, text="Lets Go!", font=("Arial", 18), command=self.close)
+        self.button.pack(padx=10, pady=10)
+
         self.root.mainloop()
+
+    def close(self):
+        self.root.destroy()
 
 InstructionFrame()
