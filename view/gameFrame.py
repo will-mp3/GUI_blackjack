cdaa7398e72f4e5e72cd64eb074c42e6d59bd908
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-04 19:40:27 -0500

    added pseudocode for table object

--- view/gameFrame.py
+++ view/gameFrame.py
@@ -5,6 +5,19 @@ class GameFrame:
     def __init__(self):
         pass
 
+    def table(self):
+        # create exit, hit, stand, and double buttons for player interaction
+        # organize table to contain:
+        # - player bank in top left, showing current chip count
+        # - the four initialized buttons with exit in the top right and the three action buttons in the bottom right, stacked
+        # - a window to display the dealers cards top center, with room to display cards to come
+        # - a similar window to displayer the players cards bottom center with the same display technology
+        # - inbetween the two sets of cards, the current bet on the table
+        pass
+
+    def exitButton(self):
+        pass
+
     def hitButton(self):
         pass
 
