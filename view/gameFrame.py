e43ded72bd1a4b626e55f3e9ba28d7f2db8a735d
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-05 20:22:02 -0500

    added hit, stand, double buttons

--- view/gameFrame.py
+++ view/gameFrame.py
@@ -25,6 +25,15 @@ class GameFrame:
         self.button = tk.Button(self.root, text="Exit", font=("Arial", 18), command=self.exit)
         self.button.grid(row=0, column=10, padx=10, pady=10)
 
+        self.button = tk.Button(self.root, text="Hit", font=("Arial", 18), command=self.hit)
+        self.button.grid(row=8, column=10, padx=10, pady=10)
+
+        self.button = tk.Button(self.root, text="Stand", font=("Arial", 18), command=self.stand)
+        self.button.grid(row=9, column=10, padx=10, pady=10)
+
+        self.button = tk.Button(self.root, text="Double", font=("Arial", 18), command=self.double)
+        self.button.grid(row=10, column=10, padx=10, pady=10)
+
         # primary label creation
         self.label = tk.Label(self.root, text="Bank", font=("Arial", 20))
         self.label.grid(row=0, column = 0, padx=10, pady=10)
