2a2b5f11d82182796ab7a5cd3dee07cae4ceec93
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-07 15:21:46 -0500

    implemented main function compability

--- view/gameFrame.py
+++ view/gameFrame.py
@@ -1,51 +1,48 @@
 import tkinter as tk
 from tkinter import messagebox
 
-class GameFrame:
+class GameFrame(tk.Frame):
 
-    def __init__(self):
-
-        self.root = tk.Tk()
-        self.root.title("Blackjack")
-        self.root.geometry("500x500")
-
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack(fill="both", expand=True)
         self.create_widgets()
+
 
     def create_widgets(self):
         # primary frame creation
-        self.frame = tk.Frame(self.root, bg="#008000", highlightbackground="#0f4d0f", highlightthickness=8)
+        self.frame = tk.Frame(self.master, bg="#008000", highlightbackground="#0f4d0f", highlightthickness=8)
         self.frame.pack()
 
         # menu bar creation
-        self.menubar = tk.Menu(self.root)
+        self.menubar = tk.Menu(self.master)
         self.optionmenu = tk.Menu(self.menubar, tearoff=0)
         self.optionmenu.add_command(label="Close", command=self.exit)
         self.optionmenu.add_separator()
         self.optionmenu.add_command(label="Change Deck Count", command=self.deckCount)
         self.menubar.add_cascade(menu=self.optionmenu, label="Options")
-        self.root.config(menu=self.menubar)
+        self.master.config(menu=self.menubar)
 
         # button creation
-        self.button = tk.Button(self.root, text="Exit", font=("Arial", 18), command=self.exit)
+        self.button = tk.Button(self.master, text="Exit", font=("Arial", 18), command=self.exit)
         self.button.pack(side="top", anchor="ne", padx=10, pady=10)
 
-        self.button = tk.Button(self.root, text="Hit", font=("Arial", 18), command=self.hit)
+        self.button = tk.Button(self.master, text="Hit", font=("Arial", 18), command=self.hit)
         self.button.pack(side="bottom", anchor="ne", padx=10, pady=10)
 
-        self.button = tk.Button(self.root, text="Stand", font=("Arial", 18), command=self.stand)
+        self.button = tk.Button(self.master, text="Stand", font=("Arial", 18), command=self.stand)
         self.button.pack(side="bottom", anchor="ne", padx=10, pady=10)
 
-        self.button = tk.Button(self.root, text="Double", font=("Arial", 18), command=self.double)
+        self.button = tk.Button(self.master, text="Double", font=("Arial", 18), command=self.double)
         self.button.pack(side="bottom", anchor="ne", padx=10, pady=10)
 
         # label creation
-        self.label = tk.Label(self.root, text="Bank", font=("Arial", 20))
+        self.label = tk.Label(self.master, text="Bank", font=("Arial", 20))
         self.label.pack(side="top", anchor="nw", padx=10, pady=10)
 
-        self.label = tk.Label(self.root, text="Bet", font=("Arial", 20))
+        self.label = tk.Label(self.master, text="Bet", font=("Arial", 20))
         self.label.pack(side="top", padx=10, pady=10)
-
-        self.root.mainloop()
 
     def table(self):
         # create exit, hit, stand, and double buttons for player interaction
@@ -59,7 +56,7 @@ class GameFrame:
 
     def exit(self):
         if messagebox.askyesno(title="Quit?", message="Do you really want to quit?"):
-            self.root.destroy()
+            self.master.destroy()
 
     def hit(self):
         pass
@@ -73,4 +70,8 @@ class GameFrame:
     def deckCount(self):
         pass
 
-GameFrame()
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.geometry("500x500")
+    app = GameFrame(master=root)
+    root.mainloop()
