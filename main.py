6ec325169fac32e97e80070848eb00ad80fea365
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-09 15:49:05 -0500

    added imports

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from model.blackjack import Blackjack
+from view.gameFrame import GameFrame
+
+import tkinter as tk
 
 def main():
     deckCount = 6
     game = Blackjack(deckCount)
-    game.run()
+    root = tk.Tk()
+    root.geometry("800x800")
+    gameState = GameFrame(master=root, game=game)
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
