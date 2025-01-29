4a05f2ec9e2f9169c5f1063f1c232679beb48baa
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-29 07:17:56 -0500

    removed run method from blackjack file

--- model/blackjack.py
+++ model/blackjack.py
@@ -82,7 +82,7 @@ class Blackjack:
         self.playerCount = 0
         self.dealerCount = 0
 
-    def _playAgain(self):
+    def playAgain(self):
 
         if self.playerChips <= 0:
             return
@@ -93,75 +93,3 @@ class Blackjack:
             return False
         else:
             self._playAgain()
-
-    def run(self):
-        self._clear()
-
-        print()
-        print("You have", self.playerChips, "chips")
-        print()
-
-        self.deck.shuffleDeck()
-        print("Welcome to the table! Please take a seat and place your bets.")
-        print()
-        while 1:
-            bet = input("BET AMOUNT: ")
-            print()
-            bet = int(bet)
-            if bet <= self.playerChips:
-                break
-            else:
-                print("Invalid bet amount.")
-                print()
-                continue
-
-        # deal cards, in this order
-        self.playerHand.append(self.deck.dealCard())
-        self.dealerHand.append(self.deck.dealCard())
-        self.playerHand.append(self.deck.dealCard())
-        self.dealerHand.append(self.deck.dealCard())
-
-        print("The dealer is showing a", self.dealerHand[0].printCard())
-        print()
-        print("You have", self.playerHand[0].printCard() + ",", self.playerHand[1].printCard())
-        self.playerCount = self._getCount("p")
-        self.dealerCount = self._getCount("d")
-
-        if self._checkBJ():
-            playerChips += bet * 1.5
-            choice = self._playAgain()
-            if not choice:
-                return
-
-        # hit or stand logic
-        while 1:
-            print()
-            move = input("Would you like to hit, stand, or double: (H/S/D) ")
-            print()
-            if move == "H":
-                val = self._hit()
-                if val:
-                    continue
-                else:
-                    break
-
-            elif move == "S":
-                break
-
-            elif move == "D":
-                if (bet * 2) <= self.playerChips:
-                    bet = bet * 2
-                    self.playerHand.append(self.deck.dealCard())
-                    print("You have", self.playerHand[0].printCard() + ",", self.playerHand[1].printCard(), ",", self.playerHand[2].printCard())
-                    print()
-                    self.playerCount = self._getCount("p")
-                    break
-            else:
-                print("Invalid response, please respond again.")
-                print()
-        
-        if self.playerCount > 21:
-            self.playerChips -= bet
-            self._playAgain()
-
-        self._dealerAction(bet)
